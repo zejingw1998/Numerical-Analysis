c1818dbbba23f477c8735de5710d3194a33e3091
Zejing wang <wangufo123@gmail.com>
2025-10-24 18:12:16 +0200

    Add files via upload

---
+++ LU decomposition.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def my_LUfactorization(A):
+    n = A.shape[0]
+    L = np.eye(n)
+    U = np.zeros_like(A)
+
+    for k in range(n):
+        # 上三角 U 的第 k 行
+        for j in range(k, n):
+            U[k, j] = A[k, j] - np.sum(L[k, :k] * U[:k, j])
+        # 下三角 L 的第 k 列
+        for i in range(k + 1, n):
+            L[i, k] = (A[i, k] - np.sum(L[i, :k] * U[:k, k])) / U[k, k]
+    return L, U
+
+n = 4
+A = np.array([[2., 1., 1., 0.],
+              [4., -6., 0., 2.],
+              [-2., 7., 2., 1.],
+              [1., 0., 3., 5.]])
+
+L, U = my_LUfactorization(A)
+print("L=\n", L)
+print("U=\n", U)
+print("LU≈A? ", np.allclose(L @ U, A))
